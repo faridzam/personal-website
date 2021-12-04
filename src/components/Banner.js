a9e64c2499c1eeef9c316b522525598cd82b8320
faridzam <skinel123@gmail.com>
2021-12-04 20:17:43 +0000

    fix assets directory

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import CTA from './CTA-button'
+import logoTrans from '../assets/logo_trans.svg'
 
 class Banner extends React.Component{
     render(){
         return(
             <div className="banner-main">
-                <img className="transparent-logo" src={"/assets/logo.svg"} alt="logo" />
+                <img className="transparent-logo" src={logoTrans} alt="logo" />
                 <div className="banner-text1">
                     <h1 className="banner-h1">DE</h1>
                     <h2 className="banner-h2">veloper<br/>signer</h2>
