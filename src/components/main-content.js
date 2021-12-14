e29b53646ced00420b286d508836fd1282970f9e
faridzam <skinel123@gmail.com>
2021-12-14 10:50:37 +0700

    add portfolios

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -2,9 +2,14 @@ import React from 'react'
 import designIcon from '../assets/design-icon.svg'
 import developmentIcon from '../assets/development-icon.svg'
 import onProcessIcon from '../assets/on-process-icon.svg'
+import PortfolioKiri from '../assets/portfolio-kiri.png'
+import PortfolioKiriKopi from '../assets/portfolio-kiri-kopi.png'
+import PortfolioSaloka from '../assets/portfolio-saloka.png'
 
 class MainContent extends React.Component{
+
     render(){
+        const {changeProject, choosenProject} = this.props;
         return(
             <div className="main-content">
                 <div className="main-content-skills">
@@ -76,6 +81,41 @@ class MainContent extends React.Component{
                                     <tr><td>3D Animation</td></tr>
                                 </tbody>
                             </table>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="main-content-portfolio">
+                    <div className="main-content-title">
+                        <h1>Portfolios.</h1>
+                    </div>
+                    <div className="container-portfolio">
+                        <div className="portfolio-projects-list">
+                            <table className="projects-list-table">
+                                <tbody className="projects-list-tbody">
+                                    <tr>
+                                        <td className={PortfolioKiri === this.props.choosenProject && "active-project"} onClick={() => changeProject(PortfolioKiri)}>
+                                            <h1>Kiri</h1>
+                                            <p>brand identity design</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={PortfolioKiriKopi === this.props.choosenProject && "active-project"} onClick={() => changeProject(PortfolioKiriKopi)}>
+                                            <h1>Kiri_Kopi</h1>
+                                            <p>Web App Development</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={PortfolioSaloka === this.props.choosenProject && "active-project"} onClick={() => changeProject(PortfolioSaloka)}>
+                                            <h1>Saloka</h1>
+                                            <p>Web App Development</p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="project-display">
+                            <img src={this.props.choosenProject} alt={this.props.choosenProject} height="720px"></img>
                         </div>
                     </div>
                 </div>
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { Banner, MainContent, Footer } from "../components";
+import PortfolioKiri from '../assets/portfolio-kiri.png'
 
 
 class Main extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            choosenProject : PortfolioKiri,
+        }
+    }
+
+    changeProject = (value) => {
+        this.setState({
+          choosenProject : value,
+        });
+      };
+
     render(){
+        const {choosenProject} = this.state;
         return(
             <div className="landing-page">
                 <Banner/>
-                <MainContent/>
+                <MainContent
+                    changeProject={this.changeProject}
+                    choosenProject = {choosenProject}
+                />
                 <Footer />
             </div>
         );
