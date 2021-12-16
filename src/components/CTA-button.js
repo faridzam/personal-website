85e4784960358d30c7f2f347cda8c0a12b6caf79
faridzam <skinel123@gmail.com>
2021-12-16 18:44:01 +0700

    deploy portfolio section

--- src/components/CTA-button.js
+++ src/components/CTA-button.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
 
 class CTA extends React.Component{
     render(){
         return(
-            <button className="cta-button">
-                <h1 className="cta-text1">GET IN TOUCH</h1>
-            </button>
+            <Link>
+                <button className="cta-button">
+                    <h1 className="cta-text1">GET IN TOUCH</h1>
+                </button>
+            </Link>
         );
     }
 }
